4d954ca1238f23878ff57000696a66b1177ac731
Rama Mahindra <rammahindra90@gmail.com>
2016-03-20 19:51:38 +0530

    First Commit
    Basic chat

---
+++ Send.py
@@ -0,0 +1,50 @@
+import socket   #for sockets
+import sys  #for exit
+ 
+try:
+    #create an AF_INET, STREAM socket (TCP)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error, msg:
+    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+    sys.exit();
+ 
+print 'Socket Created'
+ 
+host = '172.20.24.244'
+#host = socket.gethostname()
+port = 8080
+ 
+try:
+    remote_ip = socket.gethostbyname( host )
+ 
+except socket.gaierror:
+    #could not resolve
+    print 'Hostname could not be resolved. Exiting'
+    sys.exit()
+     
+print 'Ip address of ' + host + ' is ' + remote_ip
+ 
+#Connect to remote server
+s.connect((remote_ip , port))
+ 
+print 'Socket Connected to ' + host + ' on ip ' + remote_ip
+ 
+###Send some data to remote server
+##message = "You are working"
+## 
+##try :
+##    #Set the whole string
+##    s.sendall(message)
+##except socket.error:
+##    #Send failed
+##    print 'Send failed'
+##    sys.exit()
+## 
+##print 'Message send successfully'
+
+while True:
+  msg = raw_input('CLIENT >> ')
+  s.send(msg)
+  msg = s.recv(1024)
+  print 'SERVER >> ', msg
+#s.close                     # Close the socket when done
